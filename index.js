8a45ede2c22f19450850c1ab40cf4b015c2a329b
benterem <benterem@gmail.com>
2021-10-05 15:21:36 +0300

    middleware file

--- index.js
+++ index.js
@@ -6,22 +6,5 @@ const scrapePlaylist = require('./scrapers')
 
 const server = http.createServer(app)
 
-
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:', request.path)
-  console.log('Body:', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint'})
-}
-
-app.use(unknownEndpoint)
-app.use(requestLogger)
-
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT} woof woof`))
