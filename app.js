03fd300a49bc2f4842912d6680f50b9796156acc
benterem <benterem@gmail.com>
2021-10-05 15:16:47 +0300

    app file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// module.exports = app
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const playlistRouter = require('./controllers/playlist')
+// const middleware = require('./utils/')
+const logger = require('./utils/logger')
+
+app.use(cors)
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/playlist')
+module.exports = app
