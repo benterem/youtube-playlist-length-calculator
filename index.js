bbba4043b8a8d652063874a03fb48ebf67c91e8f
benterem <benterem@gmail.com>
2021-10-05 14:48:20 +0300

    middlewares

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express')
 const app = express()
 const scrapePlaylist = require('./scrapers')
@@ -6,9 +6,25 @@ const scrapePlaylist = require('./scrapers')
 const url = 'https://www.youtube.com/playlist?list=PL8dPuuaLjXtOPRKzVLY0jJY-uHOH9KVU6'
 
 app.get('/api/playlists/length', (request, response) => {
-  const urlLengthObject = scrapePlaylist(url).then(data => response.send(data));
-  // response.json(urlLengthObject)
+  scrapePlaylist(url)
+    .then(data => response.send(data))
+    .catch(e => response.status(404).end())
 })
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+app.use(requestLogger)
 
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT} woof woof`))
