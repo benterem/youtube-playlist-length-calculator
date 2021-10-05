e660724017261ce6c5f0f02d3240a091f533062c
benterem <benterem@gmail.com>
2021-10-05 15:24:19 +0300

    app use middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const playlistRouter = require('./controllers/playlist')
-// const middleware = require('./utils/')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
 app.use(cors)
@@ -10,6 +10,7 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/playlists')
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
