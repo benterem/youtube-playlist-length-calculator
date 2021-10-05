094c394956e63d1f50f9a5623ea441ffcc4f512b
benterem <benterem@gmail.com>
2021-10-05 15:26:43 +0300

    remove logger from app

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express()
 const cors = require('cors')
 const playlistRouter = require('./controllers/playlist')
 const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 
 app.use(cors)
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.use('/api/playlists')
+app.use('/api/playlists', playlistRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
