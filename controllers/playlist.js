416e8a757f311b0290a680467a2d866be9c09938
benterem <benterem@gmail.com>
2021-10-08 17:21:53 +0300

    use url param

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -3,13 +3,16 @@ const scrapePlaylist = require('../scrapers')
 const url = 'https://www.youtube.com/playlist?list=PL8dPuuaLjXtOPRKzVLY0jJY-uHOH9KVU6'
 
 playlistRouter.get('/', (request, response) => {
-  console.log('sdfad')
-  scrapePlaylist('https://www.youtube.com/watch?v=eal4-A89IWY&list=PL8dPuuaLjXtOPRKzVLY0jJY-uHOH9KVU6')
+  
+  const playlistUrl = request.body.url
+
+  scrapePlaylist(playlistUrl)
     .then(data => response.send(data))
     .catch(e => {
       console.log('in catch') 
       response.status(404).end()
     })
 })
+
 
 module.exports = playlistRouter
