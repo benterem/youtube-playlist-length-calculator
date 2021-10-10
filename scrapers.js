203f7caea9af66416319322357293035d45184c5
benterem <benterem@gmail.com>
2021-10-10 15:42:20 +0300

    handle non-playlist url

--- scrapers.js
+++ scrapers.js
@@ -6,29 +6,36 @@ async function scrapePlaylist (url) {
   console.log('in scraper')
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, {waitUntil: 'networkidle0'});
+
+
+  //if url given is of single video, go to playlist page
+  if(url.search('playlist') === -1){
+    const [a_tag] = await page.$x("//a[starts-with(@href, '/playlist')]")
+    const url_object = await a_tag.getProperty('href')
+    const url = await url_object.jsonValue()
+
+    await page.goto(url)
+  }
+
+  const spans = await page.$x('//span[@class="style-scope ytd-thumbnail-overlay-time-status-renderer"]');
+
+  const srcs = await Promise.all(spans.map(async span => await span.getProperty('innerText')));
+  const timeStamps = await Promise.all(srcs.map(async src => await src.jsonValue()));
+
+  const totalTimeInSeconds = timeStamps.reduce((a, c) => {
+    let timeArray = c.split(':');
+    return a + parseInt(timeArray[0]) * 60 + parseInt(timeArray[1]);
+  }, 0);
   
-  setTimeout( async () => {
+  browser.close();
 
-    const spans = await page.$x('//span[@class="style-scope ytd-thumbnail-overlay-time-status-renderer"]');
-
-    const srcs = await Promise.all(spans.map(async span => await span.getProperty('innerText')));
-    const timeStamps = await Promise.all(srcs.map(async src => await src.jsonValue()));
-
-    const totalTimeInSeconds = timeStamps.reduce((a, c) => {
-      let timeArray = c.split(':');
-      return a + parseInt(timeArray[0]) * 60 + parseInt(timeArray[1]);
-    }, 0);
-
-    console.log(`There are ${timeStamps.length} videos in the playlist`);
-    
-    browser.close();
-
-    return {
-      totalLengthInSeconds: totalTimeInSeconds,
-      approximateTimeInMinutes : Math.floor(totalTimeInSeconds / 60)
-    }
-  }, 2000)
+  playlistData = {
+    totalLengthInSeconds: totalTimeInSeconds,
+    approximateTimeInMinutes : Math.floor(totalTimeInSeconds / 60)
+  }
+  
+  return playlistData
 }
 
 
